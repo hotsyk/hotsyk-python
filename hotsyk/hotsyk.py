fcc8110045094ed1c280b3bca076a65a739def15
Volodymyr Hotsyk <gotsyk@gmail.com>
2015-03-17 11:06:22 +0200

    Trigger travis build

--- hotsyk/hotsyk.py
+++ hotsyk/hotsyk.py
@@ -2,8 +2,10 @@
 
 
 def cv():
+    """Return url to CV"""
     return "http://hotsyk.com/cv/"
 
 
 def talks():
+    """Return url to page with list of talk"""
     return "http://hotsyk.com/talks/"
